541ab24b0b60519838e7b52ae134245221ee587b
Tea-rex67 <quicktechrepairs247@gmail.com>
2026-04-10 23:29:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -338,3 +338,13 @@ document.querySelectorAll('.nav-links a').forEach(link => {
 });
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-link");
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      links.forEach(l => l.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
+});
